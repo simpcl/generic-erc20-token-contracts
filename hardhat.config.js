1d09d8593c7915bcf8ba3db2260dfac65df9fbba
simpcl <simpcl2008@gmail.com>
2025-12-15 10:21:45 +0800

    update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,11 +5,22 @@ dotenv.config();
 
 export default {
   networks: {
-    private: {
+    local: {
       url: "http://127.0.0.1:8545",
       accounts: [process.env.PRIVATE_KEY],
       chainId: 1337
-    }
+    },
+    memo: {
+      url: "https://chain.metamemo.one:8501",
+      accounts: [process.env.PRIVATE_KEY],
+      chainId: 985
+    },
+    bsctest: {
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545",
+      accounts: [process.env.PRIVATE_KEY],
+      chainId: 97,
+      gasPrice: 20000000000
+    },
   },
 
   solidity: {
